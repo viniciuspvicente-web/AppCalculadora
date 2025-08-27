515ed4bb93a9ff562f490693ae0e5fe448f4b837
viniciuspvicente-web <vinicius_p_vicente@estudante.sesisenai.org.br>
2025-08-27 14:05:33 -0300

    Revert "Feature multiplicacao divisao"

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,11 +1,3 @@
 public class Calculadora {
 
-    public static int multiplicacao(int a, int b) {
-        return a * b;
-    }
-    public static double divisao(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException("Divisão por zero não permitida!");
-        return (double) a / b;
-    }
-
 }
